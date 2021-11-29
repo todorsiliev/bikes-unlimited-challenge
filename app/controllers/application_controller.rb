f1604cb33255f2efc8c7bb3dc850d81905b2af9b
Todor Iliev <todor.s.iliev@gmail.com>
2021-11-29 15:10:25 +0200

    Add devise-jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  respond_to :json
   before_action :authenticate_user!
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :registerable, :confirmable, :lockable, :timeoutable, :trackable and
+  # :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
